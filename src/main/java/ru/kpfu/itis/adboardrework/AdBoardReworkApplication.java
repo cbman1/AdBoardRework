842a4113c1cf517b5a42a944e447addef435c187
cbman1999 <38330163+cbman1999@users.noreply.github.com>
2023-05-13 06:17:48 +0300

    add porject

---
+++ src/main/java/ru/kpfu/itis/adboardrework/AdBoardReworkApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.adboardrework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdBoardReworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdBoardReworkApplication.class, args);
+    }
+
+}
